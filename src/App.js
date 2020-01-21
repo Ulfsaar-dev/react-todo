32c30d573691c8209d7172a9d9574349fdc16b38
Ulfsaar-dev <wangtoby1125@outlook.com>
2020-01-22 02:59:21 +0800

    Second Repair

--- src/App.js
+++ src/App.js
@@ -1,59 +1,85 @@
-import React, { Component } from 'react'
-import TodoList from './TodoList'
-import TodoItems from './TodoItems'
+import React, { useState } from "react";
+import "./index.css";
 
-class App extends Component {
-  inputElement = React.createRef()
-  constructor() {
-    super()
-    this.state = {
-      items: [],
-      currentItem: {
-        text: '',
-        key: '',
-      },
-    }
-  }
-  deleteItem = key => {
-    const filteredItems = this.state.items.filter(item => {
-      return item.key !== key
-    })
-    this.setState({
-      items: filteredItems,
-    })
-  }
+function Todo({ todo, index, changeStateTodo}) {
+  const ButtonText = [
+    'To Start',
+    'Ongoing',
+    'Completed'
+  ];
+  const TextStyle = [
+    'Red',
+    'Green',
+    'Blue'
+  ]
 
-  handleInput = e => {
-    const itemText = e.target.value
-    const currentItem = { text: itemText, key: Date.now() }
-    this.setState({
-      currentItem,
-    })
-  }
-  addItem = e => {
-    e.preventDefault()
-    const newItem = this.state.currentItem
-    if (newItem.text !== '') {
-      const items = [...this.state.items, newItem]
-      this.setState({
-        items: items,
-        currentItem: { text: '', key: '' },
-      })
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        <TodoList
-          addItem={this.addItem}
-          inputElement={this.inputElement}
-          handleInput={this.handleInput}
-          currentItem={this.state.currentItem}
-        />
-        <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
-      </div>
-    )
-  }
+  return (
+    <div className="todo"
+      style={{ color: TextStyle[todo.Status] }}
+    >
+      {todo.text}
+      <button onClick={() => changeStateTodo(index)}>{ButtonText[todo.Status]}</button>
+    </div>
+  );
 }
 
-export default App
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Test Text",
+      Status: 0
+    }
+  ]);
+
+  const addTodo = (text, Status) => {
+    if (!Status) Status = 0;
+    const newTodos = [...todos, { text, Status }];
+    setTodos(newTodos);
+  };
+
+  const changeStateTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].Status = (newTodos[index].Status + 1) % 3;
+    
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            changeStateTodo={changeStateTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
